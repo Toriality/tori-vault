d7b9fbe37fa9279f6f9754ad4293368322c67b18
Pedro <38092988+Toriality@users.noreply.github.com>
2025-01-02 19:15:26 -0300

    .

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="flex items-center justify-center h-16">
+      <h1 className="text-3xl font-bold">Tori&#39;s Vault</h1>
+    </header>
+  );
+}
